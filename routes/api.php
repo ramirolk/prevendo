1281bd9cc02839e2be31fcad109944ba286a89f8
ramiro-mtz <ramihuel@gmail.com>
2026-08-09 02:59:31 -0300

    feat: add products CRUD API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,4 +19,12 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::apiResource('categories', CategoryController::class);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('products', ProductController::class);
 });
